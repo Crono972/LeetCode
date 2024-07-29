0386419fc08c7a0eb10ea9ae3d23d4f64e28aa49
FHocs <hocansungf@gmail.com>
2024-07-29 11:53:03 +0200

    ✨ Pb 003 - Longest Substring

---
+++ Problems/003/LongestSubstring.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace LeetCode.Problems._003;
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        var longest = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            var local = 0;
+            var hash = new HashSet<char>();
+            for (int j = i; j < s.Length; j++)
+            {
+                if (hash.Contains(s[j]))
+                {
+                    break;
+                }
+                hash.Add(s[j]);
+                local++;
+            }
+
+            longest = Math.Max(longest, local);
+        }
+
+        return longest;
+    }
+}
+
+public class SolutionTest
+{
+    [Theory]
+    [InlineData("abcabcbb", 3)]
+    [InlineData("bbbbb", 1)]
+    [InlineData("pwwkew", 3)]
+    public void TestCase(string input, int expected)
+    {
+        var sol = new Solution();
+        Assert.Equal(expected, sol.LengthOfLongestSubstring(input));
+    }
+}
